cb443719ca19cc4aef05c17ffd8a69b5ff98734f
Mauriciohnf45 <mh173509@gmail.com>
2024-10-19 08:40:58 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BemVindo from './assets/components/Bemvindo';
+
+function App() {
+  return (
+    <div className="App">
+      <BemVindo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Bemvindo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BemVindo() {
+  return <h1>Bem-vindo ao React!</h1>;
+}
+
+export default BemVindo;
